4b1587bde05078263eccf6d072e287f5b91a8e18
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2025-10-13 21:43:47 +0100

    enemy
    - fixed bug with turret enemy receiving kill blow

--- sekirodmc/scenes/actors/enemy/turret/red_360_turret.gd
+++ sekirodmc/scenes/actors/enemy/turret/red_360_turret.gd
@@ -11,9 +11,10 @@ func apply_knockback(from_position: Vector2):
 func handle_parry():
 	var amount = 1
 	if parry_meter_node: 
-		parry_meter_node.set_parry_details(amount, self)
 		parry_meter_node.global_rotation = deg_to_rad(180)
 		parry_meter_node.global_position = self.global_position + Vector2(0, -70)
+		parry_meter_node.show()
+		parry_meter_node.stun_sender()
 	pass
 
 func handle_deathblow():
